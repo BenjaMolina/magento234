fafbd7572e673119f6518dc7e39e75d979961aa6
benjadev <desarrolloti25@copicocorp.com>
2020-09-18 14:19:35 -0500

    Creando Ruta y controlador para ver todos los Banners

---
+++ app/code/CasaLum/BannerSlider/Controller/Banner/Index.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace CasaLum\BannerSlider\Controller\Banners;
+
+use Magento\Framework\App\Action\Action;
+use \Magento\Backend\App\Action\Context;
+use \Magento\Framework\Registry;
+use \Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'CasaLum_BannerSlider::all_banners';
+
+
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\Registry $coreRegistry
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        Registry $coreRegistry,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context, $coreRegistry);
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('CasaLum_BannerSlider::all_banners')
+            ->addBreadcrumb(__('Banners Sliders'), __('Banners Sliders'))
+            ->getConfig()
+            ->getTitle()
+            ->prepend(__('All Banners'));
+
+        return $resultPage;
+    }
+
+}
+
